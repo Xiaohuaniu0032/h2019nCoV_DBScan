65dd6fa34b50a9b649a5d21047a739b1f8dcf05a
longfei.fu <longfei.fu@thermofisher.com>
2022-08-05 16:46:26 +0800

    update

--- scripts/parse_nextalign.pl
+++ scripts/parse_nextalign.pl
@@ -3,14 +3,36 @@ use warnings;
 use Data::Dumper;
 use FindBin qw/$Bin/;
 
-my ($align_file,$ins_file,$outdir) = @ARGV;
+my ($align_file,$cons_fa,$ins_file,$outdir) = @ARGV;
 
 my $align_file_name       = basename($align_file);
-my $variants_file         = "$outdir/$align_file_name\.nextalign.variants.xls";
 my $variants_summary_file = "$outdir/$align_file_name\.nextalign.variants.summary.xls";
 my $similarity_file       = "$outdir/$align_file_name\.similarity.xls";
 
 
+open SUMMARY, ">$variants_summary_file" or die;
+print SUMMARY "Sample\tRef_fa_len\tConsensus_fa_len\tN_num\tN_pct\tEff_pos_start\tEff_pos_end\n";
+
+open SIM, ">$similarity_file" or die;
+print SIM "Sample\tRef_Name\tSimilarity\n";
+
+
+my @cons_name;
+my %cons_fa;
+my $name;
+
+open CONS, "$cons_fa" or die;
+while (<CONS>){
+	chomp;
+	if (/^\>/){
+		$name = $_;
+		$name =~ s/^\>//;
+		push @cons_name, $name;
+	}else{
+		$cons_fa{$name} = $_;
+	}
+}
+close CONS;
 
 # insertion信息
 # 2022-7-15.insertions.csv
@@ -77,9 +99,15 @@ if (!-d "$outdir/variants"){
 my $ref_seq_name  = "2019-nCoV";
 my $ref_seq_fa    = $seq_info{$ref_seq_name};
 
-for my $seq_name (@seq_name){
-	next if ($seq_name eq "2019-nCoV");
-	my $variants_file = "$outdir/variants/$seq_name\.variants.xls";
+
+for my $cons (@cons_name){
+	my %similarity;
+	# 只计算query seq和ref的相似度
+	# 跳过计算数据库中的序列和ref的相似度
+
+	for my $seq_name (@seq_name){
+		next if ($seq_name eq "2019-nCoV");
+		my $variants_file = "$outdir/variants/$seq_name\.variants.xls";
 
 	# 变异位点
 	open O, ">$variants_file" or die;
@@ -89,7 +117,7 @@ for my $seq_name (@seq_name){
 	for my $var (@{$vars_aref}){
 		my @var = split /\:/, $var;
 		my $new_var = join("\t",@var);
-		print O "$seq_name\t$new_var\n"; 
+		print O "$seq_name\t$new_var\n";
 	}
 	close O;
 
@@ -113,10 +141,12 @@ for my $seq_name (@seq_name){
 
 	# 计算和ref fa相似度
 	# match百分比
+	my $eff_pos_reg_aref = &get_eff_pos_region($ref_seq_fa,$q_seq);
+	my $eff_pos_start = $eff_pos_reg_aref->[0];
+	my $eff_pos_end   = $eff_pos_reg_aref->[1];
 
-
-
-
+	my $sim = &cal_similarity($ref_seq_fa,$q_seq,$eff_pos_start,$eff_pos_end);
+	$similarity{$seq_name} = $sim;
 }
 
 
@@ -191,15 +221,65 @@ sub cmp_two_seq{
 
 
 sub cal_similarity{
+	# https://www.ncbi.nlm.nih.gov/books/NBK62051/
+	# raw score OR similarity
+	my ($ref_seq,$query_seq,$eff_pos_start,$eff_pos_end) = @_;
 
+	my $eff_region_len = $eff_pos_end - $eff_pos_start + 1;
+	
+	my $start_pos = $eff_pos_start - 1; # 0-based
+	my $sub_ref_seq = substr($ref_seq, $start_pos, $eff_region_len);
+	my $sub_query_seq = substr($query_seq, $start_pos, $eff_region_len);
+
+	my @sub_ref_seq = split //, $sub_ref_seq;
+	my @sub_query_seq = split //, $sub_query_seq;
+
+	# 计算相似性
+	my $match_n = 0;
+
+	my $idx = 0;
+	for my $base (@sub_ref_seq){
+		my $ref_base = $sub_ref_seq[$idx];
+		my $query_base = $sub_query_seq[$idx];
+		if ($ref_base eq $query_base){
+			$match_n += 1;
+		}
+		$idx += 1;
+	}
+
+	my $similarity;
+	if ($match_n > 0 and $eff_region_len > 0){
+		$similarity = sprintf "%.2f", $match_n / $eff_region_len * 100;
+	}else{
+		$similarity = 0;
+	}
+
+	return($similarity);
 }
 
 
-sub cal_N_pct{
-	my ($query_seq) = @_;
+sub get_eff_pos_region{
+	my ($ref_seq,$query_seq) = @_;
+
+	my $ref_len = length($ref_seq);
+	my $query_len = length($query_seq);
+
+	#确认query序列首尾非N位置
+	my $eff_pos;
+
+	my @ref_seq   = split //, $ref_seq;
+	my @query_seq = split //, $query_seq;
+
+	my $idx = 0;
+	for my $base (@query_seq){
+		$idx += 1;
+		if ($base ne "-"){
+			push @eff_pos, $idx;
+		}
+	}
+
+	# my $eff_pos_start = $eff_pos[0]; # 1-based
+	# my $eff_pos_end   = $eff_pos[1];
+
+	return(\@eff_pos);
 }
-
-sub cal_seq_len{
-
-}
-
