6f0e9ac31d8c916607b840d67c9f9839b2f3bdbb
longfei.fu <longfei.fu@thermofisher.com>
2022-08-12 15:26:34 +0800

    update

--- 2019nCoV_DBScan_pipeline.pl
+++ 2019nCoV_DBScan_pipeline.pl
@@ -6,15 +6,15 @@ use Data::Dumper;
 use FindBin qw/$Bin/;
 use List::Util qw(sum);
 
-my ($input_fa,$hcov19_db_dir,$fa_name,$samtools_bin,$outdir);
+my ($input_fa,$input_fa_meta_file,$hcov19_db_file,$samtools_bin,$outdir);
 
 # longfei.fu
 # 2022-8-4 QIXI
 
 GetOptions(
 	"fa:s"        => \$input_fa,            # Needed
-	"dbdir:s"     => \$hcov19_db_dir,       # Needed
-	"faname:s"    => \$fa_name,             # Needed
+	"meta_f:s"    => \$input_fa_meta_file,  # Needed 
+	"dbfile:s"    => \$hcov19_db_file,      # Needed
 	"samtools:s"  => \$samtools_bin,        # Default: /usr/bin/samtools
 	"od:s"        => \$outdir,              # Needed
 	) or die "Please check your args\n";
@@ -28,45 +28,141 @@ if (not defined $samtools_bin){
 my $nextalign_bin = "$Bin/bin/nextalign";
 my $ref_fasta     = "$Bin/reference/Ion_AmpliSeq_SARS-CoV-2-Insight.Reference.fa";
 
-my $runsh = "$outdir/runsh\_$fa_name\.sh";
 
+# consensus fa样本信息
 
-############# get 2019nCoV database #############
-my @db = glob "$hcov19_db_dir/*.fasta";
-print "hcov19_db_file:\n";
-for my $file (@db){
-	print "\t$file\n";
+my @samples; # 1)remove >; 2) if seq_header has '\s+' or '/'
+my %sample_fa;
+
+my $seq_header;
+open IN, "$input_fa" or die;
+while (<IN>){
+	chomp;
+	next if /^$/;
+	if (/^\>/){
+		$seq_header = $_;
+		$seq_header =~ s/^\>//;
+		
+		# >hCoV-19/Jiangxi/JXCDC-18/2022
+		# >Sample 1277-1__2019-nCoV
+		
+		my $new_header;
+		if ($seq_header =~ /\s+/){
+			my @seq_header = split /\s+/, $seq_header;
+			$new_header = join("_",@seq_header);
+		}else{
+			$new_header = $seq_header;
+		}
+
+		if ($new_header =~ /\//){
+			my @new_header = split /\//, $new_header;
+			$seq_header = join("_",@new_header);
+		}else{
+			$seq_header = $new_header;
+		}
+
+		push @samples, $seq_header;
+	}else{
+		push @{$sample_fa{$seq_header}}, $_;
+	}
 }
-my $db_file = $db[0];
-############# get 2019nCoV database #############
+close IN;
 
-open O, ">$runsh" or die;
-# modify seq header
-my $new_input_fa = "$outdir/consensus.fasta";
-print O "perl $Bin/scripts/pre_process_input_fa.pl $input_fa $new_input_fa\n\n";
 
-# merge fa
-my $merge_fa = "$outdir/input.aln.fasta";
-print O "perl $Bin/scripts/prepare_input_fa_to_align.pl $ref_fasta $new_input_fa $db_file $merge_fa\n\n";
+# meta信息
+my @col = qw/cons_fa_name virus collect_data seq_data province length host age sex Nextstrain_clade pangolin_lineage submitting_lab submitter/;
 
-# do align
-print O "$nextalign_bin --sequences\=$merge_fa --reference\=$ref_fasta --output-dir\=$outdir --output-basename\=$fa_name --in-order\n\n";
+my %meta;
+open META, "$input_fa_meta_file" or die;
+my $meta_header = <META>;
+chomp $meta_header;
 
-# parse aln file
-my $aln_file = "$outdir/$fa_name\.aligned.fasta";
-my $ins_file = "$outdir/$fa_name\.insertions.csv";
+while (<META>){
+	chomp;
+	my @arr = split /\t/;
+	my $name = $arr[0];
 
-print O "perl $Bin/scripts/parse_nextalign.pl $aln_file $new_input_fa $ref_fasta $samtools_bin $ins_file $outdir\n\n";
+	my $new_name;
+	if ($name =~ /\s+/){
+		my @name = split /\s+/, $name;
+		$new_name = join("_",@name);
+	}else{
+		$new_name = $name;
+	}
 
-# cal similarity based on variants calling
-my $similar_file = "$outdir/$fa_name\.similarity.based_on_variant_calling.xls";
-print O "perl $Bin/scripts/find_similar_db_seq_using_variants.pl $outdir/variants $outdir/variants/2019nCoV_DB_variants $similar_file\n\n";
-`chmod 755 $runsh`;
+	my $final_name;
+	if ($new_name =~ /\//){
+		my @new_name = split /\//, $new_name;
+		$final_name = join("_",@new_name);
+	}else{
+		$final_name = $new_name;
+	}
 
-# output top10 most similar db seq info
-my $top_10_file = "$outdir/$fa_name\.similarity.based_on_variant_calling.top10.xls";
-print O "perl $Bin/scripts/get_top10_most_similar_db_seq.pl $similar_file $top_10_file\n";
-close O;
 
-`chmod 755 $runsh`;
-`$runsh`;
+	$meta{$final_name} = $_;
+}
+close META;
+
+
+
+
+
+# 每个样本新建一个目录
+for my $name (@samples){
+	if (!-d "$outdir/$name"){
+		`mkdir -p $outdir/$name`;
+	}
+
+	my @fa = @{$sample_fa{$name}};
+	my $sample_fa = join("",@fa);
+
+	# write sample fasta
+	my $sample_fa_file = "$outdir/$name/$name\.fasta";
+	
+	open O, ">$sample_fa_file" or die;
+	print O "\>$name\n";
+	print O "$sample_fa";
+	close O;
+
+	# write meta info
+	my $sample_meta_file = "$outdir/$name/$name\.metadata.csv";
+	open O, ">$sample_meta_file" or die;
+	if (exists $meta{$name}){
+		my $info = $meta{$name};
+		print O "$meta_header\n";
+		print O "$info\n";
+	}else{
+		print O "NA\tNA\tNA\n";
+	}
+	close O;
+
+	my $runsh = "$outdir/$name/runsh\_$name\.sh";
+
+	open O, ">$runsh" or die;
+	# merge fa
+	my $merge_fa = "$outdir/$name/input.aln.fasta";
+	print O "perl $Bin/scripts/prepare_input_fa_to_align.pl $ref_fasta $sample_fa_file $hcov19_db_file $merge_fa\n\n";
+
+	# do align
+	print O "$nextalign_bin --sequences\=$merge_fa --reference\=$ref_fasta --output-dir\=$outdir/$name --output-basename\=nextalign --in-order\n\n";
+
+	# parse aln file
+	my $aln_file = "$outdir/$name/nextalign.aligned.fasta";
+	my $ins_file = "$outdir/$name/nextalign.insertions.csv";
+
+	print O "perl $Bin/scripts/parse_nextalign.pl $aln_file $sample_fa_file $ref_fasta $samtools_bin $ins_file $outdir/$name\n\n";
+
+	# cal similarity based on variants calling
+	my $similar_file = "$outdir/$name/$name\.similarity.based_on_variant_calling.xls";
+	print O "perl $Bin/scripts/find_similar_db_seq_using_variants.pl $outdir/$name/variants $outdir/$name/variants/2019nCoV_DB_variants $similar_file\n\n";
+
+
+	# output top10 most similar db seq info
+	my $top_10_file = "$outdir/$name/$name\.similarity.based_on_variant_calling.top10.xls";
+	print O "perl $Bin/scripts/get_top10_most_similar_db_seq.pl $similar_file $top_10_file\n";
+	close O;
+
+	`chmod 755 $runsh`;
+	#`$runsh`;
+
+}
---
+++ test/data2_results/cmd.sh
@@ -0,0 +1,5 @@
+input_fa='/data/fulongfei/git_repo/h2019nCoV_DBScan/test/data2/1652365368436.sequences.fasta'
+meta_f='/data/fulongfei/git_repo/h2019nCoV_DBScan/test/data2/1652365368436.metadata.tsv'
+dbfile='/data/fulongfei/git_repo/h2019nCoV_DBScan/database/2019nCoV.DB.CDC.fasta'
+
+perl ../../2019nCoV_DBScan_pipeline.pl -fa $input_fa -meta_f $meta_f -dbfile $dbfile -od $PWD
