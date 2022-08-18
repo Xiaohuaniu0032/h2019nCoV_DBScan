b7a39ff0e48dfd6ea55bd89fe0234fd2ced41c4b
longfei.fu <longfei.fu@thermofisher.com>
2022-08-18 19:54:29 +0800

    update

--- test/data2_results/run_1652365368436.sh
+++ test/data2_results/run_1652365368436.sh
@@ -4,7 +4,7 @@ cat /data/fulongfei/git_repo/h2019nCoV_DBScan/reference/Ion_AmpliSeq_SARS-CoV-2-
 
 /data/fulongfei/git_repo/h2019nCoV_DBScan/bin/nextalign --sequences=/data/fulongfei/git_repo/h2019nCoV_DBScan/test/data2_results/input.aln.fasta --reference=/data/fulongfei/git_repo/h2019nCoV_DBScan/reference/Ion_AmpliSeq_SARS-CoV-2-Insight.Reference.fa --output-dir=/data/fulongfei/git_repo/h2019nCoV_DBScan/test/data2_results --output-basename=nextalign --in-order
 
-perl /data/fulongfei/git_repo/h2019nCoV_DBScan/scripts/parse_nextalign.pl /data/fulongfei/git_repo/h2019nCoV_DBScan/test/data2_results/nextalign.aligned.fasta /data/fulongfei/git_repo/h2019nCoV_DBScan/test/data2_results/input.concensus.fasta /data/fulongfei/git_repo/h2019nCoV_DBScan/reference/Ion_AmpliSeq_SARS-CoV-2-Insight.Reference.fa /usr/bin/samtools /data/fulongfei/git_repo/h2019nCoV_DBScan/test/data2_results/nextalign.insertions.csv /data/fulongfei/git_repo/h2019nCoV_DBScan/test/data2_results
+perl /data/fulongfei/git_repo/h2019nCoV_DBScan/scripts/parse_nextalign.pl /data/fulongfei/git_repo/h2019nCoV_DBScan/test/data2_results/nextalign.aligned.fasta /data/fulongfei/git_repo/h2019nCoV_DBScan/test/data2_results/input.concensus.fasta /data/fulongfei/git_repo/h2019nCoV_DBScan/reference/Ion_AmpliSeq_SARS-CoV-2-Insight.Reference.fa /root/miniconda3/bin/samtools /data/fulongfei/git_repo/h2019nCoV_DBScan/test/data2_results/nextalign.insertions.csv /data/fulongfei/git_repo/h2019nCoV_DBScan/test/data2_results
 
 perl /data/fulongfei/git_repo/h2019nCoV_DBScan/scripts/merge_query_variants.pl /data/fulongfei/git_repo/h2019nCoV_DBScan/test/data2_results/variants /data/fulongfei/git_repo/h2019nCoV_DBScan/test/data2_results/vcMerged.xls
 
